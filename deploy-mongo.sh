624d7402291cca70d2cafa0482bf5d826a7457be
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-20 19:43:16 -0700

    update volume names for latest instance

--- deploy-mongo.sh
+++ deploy-mongo.sh
@@ -18,9 +18,9 @@ gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc" |
   # create mount points mount each volume, set ownershi
   sudo mkdir /data /log /journal
 
-sudo mkfs.xfs /dev/xvdf
-sudo mkfs.xfs /dev/xvdg
-sudo mkfs.xfs /dev/xvdh
+sudo mkfs.xfs /dev/sdb
+# sudo mkfs.xfs /dev/xvdg
+# sudo mkfs.xfs /dev/xvdh
 
 echo '/dev/xvdf /data xfs defaults,auto,noatime,noexec 0 0
 /dev/xvdg /journal xfs defaults,auto,noatime,noexec 0 0
@@ -45,15 +45,15 @@ echo '* soft nofile 64000
 * hard nproc 64000' | sudo tee /etc/security/limits.d/90-mongodb.conf
 
 #set write ahead limits, make persistant
-sudo blockdev --setra 0 /dev/xvdf
+sudo blockdev --setra 0 /dev/sdb
 echo 'ACTION=="add|change", KERNEL=="xvdf", ATTR{bdi/read_ahead_kb}="0"' | sudo tee -a /etc/udev/rules.d/85-ebs.rules
 
-# Once again, repeat the above command for all required volumes (note: the device we created was named /dev/xvdf but the name used by the system is xvdf).
-sudo blockdev --setra 0 /dev/xvdg
-echo 'ACTION=="add|change", KERNEL=="xvdg", ATTR{bdi/read_ahead_kb}="0"' | sudo tee -a /etc/udev/rules.d/85-ebs.rules
+# # Once again, repeat the above command for all required volumes (note: the device we created was named /dev/xvdf but the name used by the system is xvdf).
+# sudo blockdev --setra 0 /dev/xvdg
+# echo 'ACTION=="add|change", KERNEL=="xvdg", ATTR{bdi/read_ahead_kb}="0"' | sudo tee -a /etc/udev/rules.d/85-ebs.rules
 
-sudo blockdev --setra 0 /dev/xvdh
-echo 'ACTION=="add|change", KERNEL=="xvdh", ATTR{bdi/read_ahead_kb}="0"' | sudo tee -a /etc/udev/rules.d/85-ebs.rules
+# sudo blockdev --setra 0 /dev/xvdh
+# echo 'ACTION=="add|change", KERNEL=="xvdh", ATTR{bdi/read_ahead_kb}="0"' | sudo tee -a /etc/udev/rules.d/85-ebs.rules
 
 #set keepalive time
 sudo sysctl -w net.ipv4.tcp_keepalive_time=300
